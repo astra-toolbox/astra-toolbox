50598bfd0c4959da66780fe695755e2be1a28b87
Willem Jan Palenstijn <Willem.Jan.Palenstijn@cwi.nl>
2014-10-30 17:41:58 +0100

    Refactor astra_mex_data3d
    (Modified) patch by Nicola Viganò

---
+++ matlab/mex/mexCopyDataHelpFunctions.cpp
@@ -0,0 +1,257 @@
+/*
+-----------------------------------------------------------------------
+Copyright 2013 iMinds-Vision Lab, University of Antwerp
+
+Contact: astra@ua.ac.be
+Website: http://astra.ua.ac.be
+
+
+This file is part of the
+All Scale Tomographic Reconstruction Antwerp Toolbox ("ASTRA Toolbox").
+
+The ASTRA Toolbox is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The ASTRA Toolbox is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with the ASTRA Toolbox. If not, see <http://www.gnu.org/licenses/>.
+
+-----------------------------------------------------------------------
+$Id$
+*/
+
+#include "mexCopyDataHelpFunctions.h"
+
+#include "mexHelpFunctions.h"
+
+#define HAVE_OMP
+
+#define ROUND_DOWN(x, s) ((x) & ~((s)-1))
+
+#ifdef HAVE_OMP
+#	include <omp.h>
+#endif
+
+#if defined(__SSE2__)
+# include <emmintrin.h>
+
+# define STORE_32F_64F_CORE8(in, out, count, base) \
+		{\
+			const __m128 inV0 = *((const __m128 *) &in[count + 0 + base]);\
+			const __m128 inV1 = *((const __m128 *) &in[count + 4 + base]);\
+\
+			*((__m128d *)&out[count + 0 + base]) = _mm_cvtps_pd(inV0);\
+			*((__m128d *)&out[count + 2 + base]) = _mm_cvtps_pd(_mm_movehl_ps(inV0, inV0));\
+\
+			*((__m128d *)&out[count + 4 + base]) = _mm_cvtps_pd(inV1);\
+			*((__m128d *)&out[count + 6 + base]) = _mm_cvtps_pd(_mm_movehl_ps(inV1, inV1));\
+		}
+# define STORE_64F_32F_CORE8(in, out, count, base) \
+		{\
+			const __m128d inV0 = *((const __m128d *) &in[count + 0 + base]);\
+			const __m128d inV1 = *((const __m128d *) &in[count + 2 + base]);\
+\
+			const __m128d inV2 = *((const __m128d *) &in[count + 4 + base]);\
+			const __m128d inV3 = *((const __m128d *) &in[count + 6 + base]);\
+\
+			*((__m128 *)&out[count + 0 + base]) = _mm_movelh_ps(_mm_cvtpd_ps(inV0), _mm_cvtpd_ps(inV1));\
+			*((__m128 *)&out[count + 4 + base]) = _mm_movelh_ps(_mm_cvtpd_ps(inV2), _mm_cvtpd_ps(inV3));\
+		}
+# define STORE_32F_32F_CORE8(in, out, count, base) \
+		{\
+			*((__m128 *)&out[count + 0 + base]) = *((const __m128 *)&in[count + 0 + base]);\
+			*((__m128 *)&out[count + 4 + base]) = *((const __m128 *)&in[count + 4 + base]);\
+		}
+# define STORE_CONST_32F_CORE8(val, out, count, base) \
+		{\
+			*((__m128 *)&out[count + 0 + base]) = val;\
+			*((__m128 *)&out[count + 4 + base]) = val;\
+		}
+#else
+# define STORE_32F_64F_CORE8(in, out, count, base) \
+		{\
+			out[count + 0 + base] = (double)in[count + 0 + base];\
+			out[count + 1 + base] = (double)in[count + 1 + base];\
+			out[count + 2 + base] = (double)in[count + 2 + base];\
+			out[count + 3 + base] = (double)in[count + 3 + base];\
+			out[count + 4 + base] = (double)in[count + 4 + base];\
+			out[count + 5 + base] = (double)in[count + 5 + base];\
+			out[count + 6 + base] = (double)in[count + 6 + base];\
+			out[count + 7 + base] = (double)in[count + 7 + base];\
+		}
+# define STORE_64F_32F_CORE8(in, out, count, base) \
+		{\
+			out[count + 0 + base] = (float)in[count + 0 + base];\
+			out[count + 1 + base] = (float)in[count + 1 + base];\
+			out[count + 2 + base] = (float)in[count + 2 + base];\
+			out[count + 3 + base] = (float)in[count + 3 + base];\
+			out[count + 4 + base] = (float)in[count + 4 + base];\
+			out[count + 5 + base] = (float)in[count + 5 + base];\
+			out[count + 6 + base] = (float)in[count + 6 + base];\
+			out[count + 7 + base] = (float)in[count + 7 + base];\
+		}
+# define STORE_32F_32F_CORE8(in, out, count, base) \
+		{\
+			out[count + 0 + base] = in[count + 0 + base];\
+			out[count + 1 + base] = in[count + 1 + base];\
+			out[count + 2 + base] = in[count + 2 + base];\
+			out[count + 3 + base] = in[count + 3 + base];\
+			out[count + 4 + base] = in[count + 4 + base];\
+			out[count + 5 + base] = in[count + 5 + base];\
+			out[count + 6 + base] = in[count + 6 + base];\
+			out[count + 7 + base] = in[count + 7 + base];\
+		}
+#endif
+
+const char * warnDataTypeNotSupported = "Data type not supported: nothing was copied";
+
+void
+_copyMexToCFloat32Array(const mxArray * const inArray, astra::float32 * const out)
+{
+	const long long tot_size = mxGetNumberOfElements(inArray);
+	const long long block = 32;
+	const long long totRoundedPixels = ROUND_DOWN(tot_size, block);
+
+	if (mxIsDouble(inArray)) {
+		const double * const pdMatlabData = mxGetPr(inArray);
+
+#pragma omp for nowait
+		for (long long count = 0; count < totRoundedPixels; count += block) {
+			STORE_64F_32F_CORE8(pdMatlabData, out, count,  0);
+			STORE_64F_32F_CORE8(pdMatlabData, out, count,  8);
+			STORE_64F_32F_CORE8(pdMatlabData, out, count, 16);
+			STORE_64F_32F_CORE8(pdMatlabData, out, count, 24);
+		}
+#pragma omp for nowait
+		for (long long count = totRoundedPixels; count < tot_size; count++) {
+			out[count] = pdMatlabData[count];
+		}
+	}
+	else if (mxIsSingle(inArray)) {
+		const float * const pfMatlabData = (const float *)mxGetData(inArray);
+
+#pragma omp for nowait
+		for (long long count = 0; count < totRoundedPixels; count += block) {
+			STORE_32F_32F_CORE8(pfMatlabData, out, count,  0);
+			STORE_32F_32F_CORE8(pfMatlabData, out, count,  8);
+			STORE_32F_32F_CORE8(pfMatlabData, out, count, 16);
+			STORE_32F_32F_CORE8(pfMatlabData, out, count, 24);
+		}
+#pragma omp for nowait
+		for (long long count = totRoundedPixels; count < tot_size; count++) {
+			out[count] = pfMatlabData[count];
+		}
+	}
+	else {
+#pragma omp single nowait
+		mexWarnMsgIdAndTxt("ASTRA_MEX:wrong_datatype", warnDataTypeNotSupported);
+	}
+}
+
+void
+_copyCFloat32ArrayToMex(const astra::float32 * const in, mxArray * const outArray)
+{
+	const long long tot_size = mxGetNumberOfElements(outArray);
+	const long long block = 32;
+	const long long tot_rounded_size = ROUND_DOWN(tot_size, block);
+
+	if (mxIsDouble(outArray)) {
+		double * const pdMatlabData = mxGetPr(outArray);
+
+#pragma omp for nowait
+		for (long long count = 0; count < tot_rounded_size; count += block) {
+			STORE_32F_64F_CORE8(in, pdMatlabData, count,  0);
+			STORE_32F_64F_CORE8(in, pdMatlabData, count,  8);
+			STORE_32F_64F_CORE8(in, pdMatlabData, count, 16);
+			STORE_32F_64F_CORE8(in, pdMatlabData, count, 24);
+		}
+#pragma omp for nowait
+		for (long long count = tot_rounded_size; count < tot_size; count++) {
+			pdMatlabData[count] = in[count];
+		}
+	}
+	else if (mxIsSingle(outArray)) {
+		float * const pfMatlabData = (float *) mxGetData(outArray);
+
+#pragma omp for nowait
+		for (long long count = 0; count < tot_rounded_size; count += block) {
+			STORE_32F_32F_CORE8(in, pfMatlabData, count,  0);
+			STORE_32F_32F_CORE8(in, pfMatlabData, count,  8);
+			STORE_32F_32F_CORE8(in, pfMatlabData, count, 16);
+			STORE_32F_32F_CORE8(in, pfMatlabData, count, 24);
+		}
+#pragma omp for nowait
+		for (long long count = tot_rounded_size; count < tot_size; count++) {
+			pfMatlabData[count] = in[count];
+		}
+	}
+	else {
+#pragma omp single nowait
+		mexWarnMsgIdAndTxt("ASTRA_MEX:wrong_datatype", warnDataTypeNotSupported);
+	}
+}
+
+void
+_initializeCFloat32Array(const astra::float32 & val, astra::float32 * const out,
+		const size_t & tot_size)
+{
+#ifdef __SSE2__
+	const long long block = 32;
+	const long long tot_rounded_size = ROUND_DOWN(tot_size, block);
+
+	const __m128 vecVal = _mm_set1_ps(val);
+
+	{
+#pragma omp for nowait
+		for (long long count = 0; count < tot_rounded_size; count += block) {
+			STORE_CONST_32F_CORE8(vecVal, out, count,  0);
+			STORE_CONST_32F_CORE8(vecVal, out, count,  8);
+			STORE_CONST_32F_CORE8(vecVal, out, count, 16);
+			STORE_CONST_32F_CORE8(vecVal, out, count, 24);
+		}
+#else
+	const long long tot_rounded_size = 0;
+	{
+#endif
+#pragma omp for nowait
+		for (long long count = tot_rounded_size; count < (long long)tot_size; count++) {
+			out[count] = val;
+		}
+	}
+}
+
+void
+copyMexToCFloat32Array(const mxArray * const in,
+		astra::float32 * const out, const size_t &tot_size)
+{
+#pragma omp parallel
+	{
+		// fill with scalar value
+		if (mex_is_scalar(in)) {
+			astra::float32 fValue = 0.f;
+			if (!mxIsEmpty(in)) {
+				fValue = (astra::float32)mxGetScalar(in);
+			}
+			_initializeCFloat32Array(fValue, out, tot_size);
+		}
+		// fill with array value
+		else {
+			_copyMexToCFloat32Array(in, out);
+		}
+	}
+}
+
+void
+copyCFloat32ArrayToMex(const float * const in, mxArray * const outArray)
+{
+#pragma omp parallel
+	{
+		_copyCFloat32ArrayToMex(in, outArray);
+	}
+}
---
+++ matlab/mex/mexDataManagerHelpFunctions.cpp
@@ -0,0 +1,371 @@
+/*
+-----------------------------------------------------------------------
+Copyright 2013 iMinds-Vision Lab, University of Antwerp
+
+Contact: astra@ua.ac.be
+Website: http://astra.ua.ac.be
+
+
+This file is part of the
+All Scale Tomographic Reconstruction Antwerp Toolbox ("ASTRA Toolbox").
+
+The ASTRA Toolbox is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The ASTRA Toolbox is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with the ASTRA Toolbox. If not, see <http://www.gnu.org/licenses/>.
+
+-----------------------------------------------------------------------
+$Id$
+*/
+
+#include "mexDataManagerHelpFunctions.h"
+
+#include "mexHelpFunctions.h"
+
+#include "astra/ParallelProjectionGeometry3D.h"
+#include "astra/ParallelVecProjectionGeometry3D.h"
+#include "astra/ConeProjectionGeometry3D.h"
+#include "astra/ConeVecProjectionGeometry3D.h"
+#include "astra/Float32VolumeData3DMemory.h"
+#include "astra/Float32ProjectionData3DMemory.h"
+
+#define USE_MATLAB_UNDOCUMENTED
+
+#ifdef USE_MATLAB_UNDOCUMENTED
+extern "C" {
+mxArray *mxCreateSharedDataCopy(const mxArray *pr);
+bool mxUnshareArray(const mxArray *pr, const bool noDeepCopy);
+mxArray *mxUnreference(const mxArray *pr);
+#if 0
+// Unsupported in Matlab R2014b
+bool mxIsSharedArray(const mxArray *pr);
+#endif
+}
+
+class CFloat32CustomMemoryMatlab3D : public astra::CFloat32CustomMemory {
+public:
+	// offset allows linking the data object to a sub-volume (in the z direction)
+	// offset is measured in floats.
+	CFloat32CustomMemoryMatlab3D(const mxArray* _pArray, bool bUnshare, size_t iOffset)
+	{
+		// Convert from slice to offset
+		mwSize dims[3];
+		get3DMatrixDims(_pArray, dims);
+		iOffset *= dims[0];
+		iOffset *= dims[1];
+
+		//fprintf(stderr, "Passed:\narray: %p\tdata: %p\n", (void*)_pArray, (void*)mxGetData(_pArray));
+		// First unshare the input array, so that we may modify it.
+		if (bUnshare) {
+#if 0
+			// Unsupported in Matlab R2014b
+			if (mxIsSharedArray(_pArray)) {
+				fprintf(stderr, "Performance note: unsharing shared array in link\n");
+			}
+#endif
+			mxUnshareArray(_pArray, false);
+			//fprintf(stderr, "Unshared:\narray: %p\tdata: %p\n", (void*)_pArray, (void*)mxGetData(_pArray));
+		}
+		// Then create a (persistent) copy so the data won't be deleted
+		// or changed.
+		m_pLink = mxCreateSharedDataCopy(_pArray);
+		//fprintf(stderr, "SharedDataCopy:\narray: %p\tdata: %p\n", (void*)m_pLink, (void*)mxGetData(m_pLink));
+		mexMakeArrayPersistent(m_pLink);
+		m_fPtr = (float *)mxGetData(m_pLink);
+		m_fPtr += iOffset;
+	}
+	virtual ~CFloat32CustomMemoryMatlab3D() {
+		// destroy the shared array
+		//fprintf(stderr, "Destroy:\narray: %p\tdata: %p\n", (void*)m_pLink, (void*)mxGetData(m_pLink));
+		mxDestroyArray(m_pLink);
+	}
+private:
+	mxArray* m_pLink;
+};
+#endif
+
+//-----------------------------------------------------------------------------------------
+bool
+checkID(const astra::int32 & id, astra::CFloat32Data3DMemory *& pDataObj)
+{
+	pDataObj = dynamic_cast<astra::CFloat32Data3DMemory *>(
+			astra::CData3DManager::getSingleton().get(id) );
+	return (pDataObj && pDataObj->isInitialized());
+}
+
+//-----------------------------------------------------------------------------------------
+bool
+checkDataType(const mxArray * const in)
+{
+	return (mex_is_scalar(in) || mxIsDouble(in) || mxIsSingle(in));
+}
+
+//-----------------------------------------------------------------------------------------
+bool
+checkStructs(const mxArray * const in)
+{
+	return mxIsStruct(in);
+}
+
+//-----------------------------------------------------------------------------------------
+bool
+checkDataSize(const mxArray * const mArray,
+		const astra::CProjectionGeometry3D * const geom)
+{
+	mwSize dims[3];
+	get3DMatrixDims(mArray, dims);
+	return (geom->getDetectorColCount() == dims[0]
+			&& geom->getProjectionCount() == dims[1]
+			&& geom->getDetectorRowCount() == dims[2]);
+}
+
+//-----------------------------------------------------------------------------------------
+bool
+checkDataSize(const mxArray * const mArray,
+		const astra::CVolumeGeometry3D * const geom)
+{
+	mwSize dims[3];
+	get3DMatrixDims(mArray, dims);
+	return (geom->getGridColCount() == dims[0]
+			&& geom->getGridRowCount() == dims[1]
+			&& geom->getGridSliceCount() == dims[2]);
+}
+
+//-----------------------------------------------------------------------------------------
+bool
+checkDataSize(const mxArray * const mArray,
+		const astra::CProjectionGeometry3D * const geom,
+		const mwIndex & zOffset)
+{
+	mwSize dims[3];
+	get3DMatrixDims(mArray, dims);
+	return (geom->getDetectorColCount() == dims[0]
+			&& geom->getProjectionCount() == dims[1]
+			&& (zOffset + geom->getDetectorRowCount()) <= dims[2]);
+}
+
+//-----------------------------------------------------------------------------------------
+bool
+checkDataSize(const mxArray * const mArray,
+		const astra::CVolumeGeometry3D * const geom,
+		const mwIndex & zOffset)
+{
+	mwSize dims[3];
+	get3DMatrixDims(mArray, dims);
+	return (geom->getGridColCount() == dims[0]
+			&& geom->getGridRowCount() == dims[1]
+			&& (zOffset + geom->getGridSliceCount()) <= dims[2]);
+}
+
+//-----------------------------------------------------------------------------------------
+void
+updateStatistics(const std::vector<astra::CFloat32Data3DMemory *> & vecIn)
+{
+	const size_t tot_size = vecIn.size();
+	for (size_t count = 0; count < tot_size; count++)
+	{
+		vecIn[count]->updateStatistics();
+	}
+}
+
+//-----------------------------------------------------------------------------------------
+void
+getDataPointers(const std::vector<astra::CFloat32Data3DMemory *> & vecIn,
+		std::vector<astra::float32 *> & vecOut)
+{
+	const size_t tot_size = vecIn.size();
+	vecOut.resize(tot_size);
+	for (size_t count = 0; count < tot_size; count++)
+	{
+		vecOut[count] = vecIn[count]->getData();
+	}
+}
+
+//-----------------------------------------------------------------------------------------
+void
+getDataSizes(const std::vector<astra::CFloat32Data3DMemory *> & vecIn,
+		std::vector<size_t> & vecOut)
+{
+	const size_t tot_size = vecIn.size();
+	vecOut.resize(tot_size);
+	for (size_t count = 0; count < tot_size; count++)
+	{
+		vecOut[count] = vecIn[count]->getSize();
+	}
+}
+
+//-----------------------------------------------------------------------------------------
+astra::CFloat32Data3DMemory *
+allocateDataObject(const std::string & sDataType,
+		const mxArray * const geometry, const mxArray * const data,
+		const mxArray * const unshare, const mxArray * const zIndex)
+{
+	astra::CFloat32Data3DMemory* pDataObject3D = NULL;
+
+	bool bUnshare = true;
+	if (unshare)
+	{
+		if (!mex_is_scalar(unshare))
+		{
+			mexErrMsgTxt("Argument 5 (read-only) must be scalar");
+			return NULL;
+		}
+		// unshare the array if we're not linking read-only
+		bUnshare = !(bool)mxGetScalar(unshare);
+	}
+
+	mwIndex iZ = 0;
+	if (zIndex)
+	{
+		if (!mex_is_scalar(zIndex))
+		{
+			mexErrMsgTxt("Argument 6 (Z) must be scalar");
+			return NULL;
+		}
+		iZ = (mwSignedIndex)mxGetScalar(zIndex);
+	}
+
+	// SWITCH DataType
+	if (sDataType == "-vol")
+	{
+		// Read geometry
+		astra::XMLDocument* xml = struct2XML("VolumeGeometry", geometry);
+		if (!xml) {
+			return NULL;
+		}
+		astra::Config cfg;
+		cfg.self = xml->getRootNode();
+
+		astra::CVolumeGeometry3D* pGeometry = new astra::CVolumeGeometry3D();
+		if (!pGeometry->initialize(cfg))
+		{
+			mexErrMsgTxt("Geometry class not initialized. \n");
+			delete pGeometry;
+			delete xml;
+			return NULL;
+		}
+		delete xml;
+
+		// If data is specified, check dimensions
+		if (data && !mex_is_scalar(data))
+		{
+			if (! (zIndex
+					? checkDataSize(data, pGeometry, iZ)
+					: checkDataSize(data, pGeometry)) )
+			{
+				mexErrMsgTxt("The dimensions of the data do not match those specified in the geometry. \n");
+				delete pGeometry;
+				return NULL;
+			}
+		}
+
+		// Initialize data object
+#ifdef USE_MATLAB_UNDOCUMENTED
+		if (unshare) {
+			CFloat32CustomMemoryMatlab3D* pHandle =
+					new CFloat32CustomMemoryMatlab3D(data, bUnshare, iZ);
+
+			// Initialize data object
+			pDataObject3D = new astra::CFloat32VolumeData3DMemory(pGeometry, pHandle);
+		}
+		else
+		{
+			pDataObject3D = new astra::CFloat32VolumeData3DMemory(pGeometry);
+		}
+#else
+		pDataObject3D = new astra::CFloat32VolumeData3DMemory(pGeometry);
+#endif
+		delete pGeometry;
+	}
+	else if (sDataType == "-sino" || sDataType == "-proj3d" || sDataType == "-sinocone")
+	{
+		// Read geometry
+		astra::XMLDocument* xml = struct2XML("ProjectionGeometry", geometry);
+		if (!xml) {
+			return NULL;
+		}
+		astra::Config cfg;
+		cfg.self = xml->getRootNode();
+
+		// FIXME: Change how the base class is created. (This is duplicated
+		// in Projector2D.cpp.)
+		std::string type = cfg.self->getAttribute("type");
+		astra::CProjectionGeometry3D* pGeometry = 0;
+		if (type == "parallel3d") {
+			pGeometry = new astra::CParallelProjectionGeometry3D();
+		} else if (type == "parallel3d_vec") {
+			pGeometry = new astra::CParallelVecProjectionGeometry3D();
+		} else if (type == "cone") {
+			pGeometry = new astra::CConeProjectionGeometry3D();
+		} else if (type == "cone_vec") {
+			pGeometry = new astra::CConeVecProjectionGeometry3D();
+		} else {
+			mexErrMsgTxt("Invalid geometry type.\n");
+			return NULL;
+		}
+
+		if (!pGeometry->initialize(cfg)) {
+			mexErrMsgTxt("Geometry class not initialized. \n");
+			delete pGeometry;
+			delete xml;
+			return NULL;
+		}
+		delete xml;
+
+		// If data is specified, check dimensions
+		if (data && !mex_is_scalar(data))
+		{
+			if (! (zIndex
+					? checkDataSize(data, pGeometry, iZ)
+					: checkDataSize(data, pGeometry)) )
+			{
+				mexErrMsgTxt("The dimensions of the data do not match those specified in the geometry. \n");
+				delete pGeometry;
+				return NULL;
+			}
+		}
+
+		// Initialize data object
+#ifdef USE_MATLAB_UNDOCUMENTED
+		if (unshare)
+		{
+			CFloat32CustomMemoryMatlab3D* pHandle =
+					new CFloat32CustomMemoryMatlab3D(data, bUnshare, iZ);
+
+			// Initialize data object
+			pDataObject3D = new astra::CFloat32ProjectionData3DMemory(pGeometry, pHandle);
+		}
+		else
+		{
+			pDataObject3D = new astra::CFloat32ProjectionData3DMemory(pGeometry);
+		}
+#else
+		pDataObject3D = new astra::CFloat32ProjectionData3DMemory(pGeometry);
+#endif
+		delete pGeometry;
+	}
+	else
+	{
+		mexErrMsgTxt("Invalid datatype.  Please specify '-vol' or '-proj3d'. \n");
+		return NULL;
+	}
+
+	// Check initialization
+	if (!pDataObject3D->isInitialized())
+	{
+		mexErrMsgTxt("Couldn't initialize data object.\n");
+		delete pDataObject3D;
+		return NULL;
+	}
+
+	return pDataObject3D;
+}
+
