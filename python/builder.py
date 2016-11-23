9b975cbf8b4771ad9f170f5d56c611e382fcf6e9
Holger Kohr <h.kohr@cwi.nl>
2016-11-23 10:21:55 +0100

    Separate C++ and python builds & make conda work nicely
    - make builder (= advanced user or us ourselves) choose compilers
      and CUDA
    - add a check for the C++11 flag for nvcc to work around the
      infamous boost bug if necessary
    - use conda boost to build the C++ library
    - simplify python bindings conda recipe to only build the bindings;
      the C++ library is now a build and runtime dependency
    - add runtime dependencies to python bindings recipe
    - some small adjustments to builder.py

--- python/builder.py
+++ python/builder.py
@@ -21,81 +21,72 @@
 # You should have received a copy of the GNU General Public License
 # along with the ASTRA Toolbox. If not, see <http://www.gnu.org/licenses/>.
 #
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
-import sys
 import os
 import numpy as np
 
-from distutils.version import LooseVersion
 from distutils.core import setup
-from distutils.extension import Extension
+from pkg_resources import parse_version
 
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 import Cython
-if LooseVersion(Cython.__version__)<LooseVersion('0.13'): raise ImportError("Cython version should be at least 0.13")
-usecuda=False
-try:
-    if os.environ['CPPFLAGS'].find('-DASTRA_CUDA')!=-1:
-        usecuda=True
-except KeyError:
-    pass
-try:
-    if os.environ['CL'].find('/DASTRA_CUDA')!=-1:
-        usecuda=True
-except KeyError:
-    pass
+if parse_version(Cython.__version__) < parse_version('0.13'):
+    raise ImportError('Cython version should be at least 0.13')
 
+use_cuda = ('-DASTRA_CUDA' in os.environ.get('CPPFLAGS', '') or
+            '/DASTRA_CUDA' in os.environ.get('CL', ''))
 
-cfgToWrite = 'DEF HAVE_CUDA=' + str(usecuda) + "\n"
-cfgHasToBeUpdated = True
+self_path = os.path.dirname(os.path.abspath(__file__))
+
+cfg_string = 'DEF HAVE_CUDA=' + str(use_cuda) + '\n'
+update_cfg = True
 try:
-    cfg = open('astra/config.pxi','r')
-    cfgIn = cfg.read()
-    cfg.close()
-    if cfgIn==cfgToWrite:
-        cfgHasToBeUpdated = False
+    with open(os.path.join(self_path, 'astra', 'config.pxi'), 'r') as cfg:
+        cfg_fromfile = cfg.read()
+    if cfg_fromfile == cfg_string:
+        update_cfg = False
 except IOError:
     pass
 
-if cfgHasToBeUpdated:
-    cfg = open('astra/config.pxi','w')
-    cfg.write(cfgToWrite)
-    cfg.close()
+if update_cfg:
+    with open(os.path.join(self_path, 'astra', 'config.pxi'), 'w') as cfg:
+        cfg.write(cfg_string)
 
+pkgdata = {}
+if os.environ.get('ASTRA_INSTALL_LIBRARY_AS_DATA', ''):
+    pkgdata['astra'] = [os.environ['ASTRA_INSTALL_LIBRARY_AS_DATA']]
 
-pkgdata = { }
-try:
-    if os.environ['ASTRA_INSTALL_LIBRARY_AS_DATA']:
-        pkgdata['astra'] = [os.environ['ASTRA_INSTALL_LIBRARY_AS_DATA']]
-except KeyError:
-    pass
+cmdclass = {}
+ext_modules = []
 
-cmdclass = { }
-ext_modules = [ ]
-
-ext_modules = cythonize("astra/*.pyx", language_level=2)
-cmdclass = { 'build_ext': build_ext }
+ext_modules = cythonize(os.path.join(self_path, 'astra', '*.pyx'),
+                        language_level=2)
+cmdclass = {'build_ext': build_ext}
 
 for m in ext_modules:
-  if m.name == 'astra.plugin_c':
-    m.sources.append('astra/src/PythonPluginAlgorithm.cpp')
+    if m.name == 'astra.plugin_c':
+        m.sources.append(os.path.join(self_path, 'astra', 'src',
+                                      'PythonPluginAlgorithm.cpp'))
 
-setup (name = 'astra-toolbox',
-       version = '1.7.1',
-       description = 'Python interface to the ASTRA-Toolbox',
-       author='D.M. Pelt',
-       author_email='D.M.Pelt@cwi.nl',
-       url='http://sf.net/projects/astra-toolbox',
-       #ext_package='astra',
-       #ext_modules = cythonize(Extension("astra/*.pyx",extra_compile_args=extra_compile_args,extra_linker_args=extra_compile_args)),
-       license='GPLv3',
-       ext_modules = ext_modules,
-       include_dirs=[np.get_include()],
-       cmdclass = cmdclass,
-       #ext_modules = [Extension("astra","astra/astra.pyx")],
-       packages=['astra', 'astra.plugins'],
-       package_data=pkgdata,
-       requires=["numpy"],
-	)
+setup(name='astra-toolbox',
+      version='1.7.1',
+      description='Python interface to the ASTRA Toolbox',
+      author='D.M. Pelt',
+      author_email='D.M.Pelt@cwi.nl',
+      url='https://github.com/astra-toolbox/astra-toolbox',
+      # ext_package='astra',
+      # ext_modules = cythonize(
+      #     Extension("astra/*.pyx",
+      #               extra_compile_args=extra_compile_args,
+      #               extra_linker_args=extra_compile_args)),
+      license='GPLv3',
+      ext_modules=ext_modules,
+      include_dirs=[np.get_include()],
+      cmdclass=cmdclass,
+      # ext_modules = [Extension("astra","astra/astra.pyx")],
+      packages=['astra', 'astra.plugins'],
+      package_data=pkgdata,
+      requires=['numpy', 'scipy', 'six'],
+      )
--- python/conda/build.sh
+++ python/conda/build.sh
@@ -1,8 +1,4 @@
-cd build/linux
-./autogen.sh
-./configure --with-python --with-cuda=$CUDA_ROOT --prefix=$PREFIX
-if [ $MAKEOPTS == '<UNDEFINED>' ]
-  then
-    MAKEOPTS=""
-fi
-make $MAKEOPTS install
+#!/bin/sh
+
+cd $SRC_DIR/python/
+CPPFLAGS="-DASTRA_CUDA -DASTRA_PYTHON $CPPFLAGS -I$SRC_DIR/ -I$SRC_DIR/include -I$CUDA_ROOT/include" CC=$CC python ./builder.py build install
