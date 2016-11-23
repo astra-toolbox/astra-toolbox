eeffd2d9748b8912b384a5764b808f5bfc850ade
Holger Kohr <h.kohr@cwi.nl>
2016-11-23 10:21:55 +0100

    Separate C++ and python builds & make conda work nicely
    - make builder (= advanced user or us ourselves) choose compilers
      and CUDA
    - add a check for the C++11 flag for nvcc to work around the
      infamous boost bug if necessary
    - use conda boost to build the C++ library
    - simplify python bindings conda recipe to only build the bindings;
      the C++ library is now a build and runtime dependency
    - add runtime dependencies to python bindings recipe
    - some small adjustments to builder.py

--- python/conda/libastra/build.sh
+++ python/conda/libastra/build.sh
@@ -1,15 +1,23 @@
-cd build/linux
-./autogen.sh
-./configure --with-cuda=$CUDA_ROOT --prefix=$PREFIX
-if [ $MAKEOPTS == '<UNDEFINED>' ]
-  then
-    MAKEOPTS=""
-fi
-make $MAKEOPTS install-libraries
+#!/bin/sh
+
+cd $SRC_DIR/build/linux
+
+$SRC_DIR/build/linux/autogen.sh
+
+# Add C++11 to compiler flags if nvcc supports it, mostly to work around a boost bug
+NVCC=$CUDA_ROOT/bin/nvcc
+echo "int main(){return 0;}" > $CONDA_PREFIX/test.cu
+$NVCC $CONDA_PREFIX/test.cu -ccbin $CC --std=c++11 -o $CONDA_PREFIX/test.out > /dev/null && EXTRA_NVCCFLAGS="--std=c++11" || /bin/true
+rm -f $CONDA_PREFIX/test.out
+
+$SRC_DIR/build/linux/configure --with-install-type=prefix --with-cuda=$CUDA_ROOT --prefix=$CONDA_PREFIX NVCCFLAGS="-ccbin $CC $EXTRA_NVCCFLAGS" CC=$CC CXX=$CXX CFLAGS="-I$CONDA_PREFIX/include/boost" CXXFLAGS="-I$CONDA_PREFIX/include/boost"
+
+make install-libraries
+
 LIBPATH=lib
 if [ $ARCH == 64 ]
   then
     LIBPATH+=64
 fi
-cp -P $CUDA_ROOT/$LIBPATH/libcudart.so.* $PREFIX/lib
-cp -P $CUDA_ROOT/$LIBPATH/libcufft.so.* $PREFIX/lib
+cp -P $CUDA_ROOT/$LIBPATH/libcudart.so.* $CONDA_PREFIX/lib
+cp -P $CUDA_ROOT/$LIBPATH/libcufft.so.* $CONDA_PREFIX/lib
