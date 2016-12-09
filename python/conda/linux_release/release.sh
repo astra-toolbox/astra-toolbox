25f38c8ac5275ff88cc1245ab475510864543ceb
Willem Jan Palenstijn <wjp@usecode.org>
2016-12-09 11:37:26 +0100

    Build conda packages for multiple conda versions

--- python/conda/linux_release/release.sh
+++ python/conda/linux_release/release.sh
@@ -6,6 +6,7 @@ D=`mktemp -d`
 [ -f buildenv/Miniconda3-4.2.12-Linux-x86_64.sh ] || (cd buildenv; wget https://repo.continuum.io/miniconda/Miniconda3-4.2.12-Linux-x86_64.sh )
 
 docker build -t astra-build-env buildenv
+#docker build --no-cache --build-arg=BUILD_NUMBER=0 -t astra-builder builder
 docker build --no-cache -t astra-builder builder
 
 docker run --name astra-build-cnt -v $D:/out:z astra-builder /bin/bash -c "cp /root/miniconda3/conda-bld/linux-64/*astra* /out"
