76bd01e47e4fc76b16d65d9cfc0216347951c518
Willem Jan Palenstijn <Willem.Jan.Palenstijn@cwi.nl>
2017-01-23 14:13:22 +0100

    Silence errors from nvcc test run

--- python/conda/libastra/build.sh
+++ python/conda/libastra/build.sh
@@ -7,7 +7,7 @@ $SRC_DIR/build/linux/autogen.sh
 # Add C++11 to compiler flags if nvcc supports it, mostly to work around a boost bug
 NVCC=$CUDA_ROOT/bin/nvcc
 echo "int main(){return 0;}" > $CONDA_PREFIX/test.cu
-$NVCC $CONDA_PREFIX/test.cu -ccbin $CC --std=c++11 -o $CONDA_PREFIX/test.out > /dev/null && EXTRA_NVCCFLAGS="--std=c++11" || true
+$NVCC $CONDA_PREFIX/test.cu -ccbin $CC --std=c++11 -o $CONDA_PREFIX/test.out > /dev/null 2>&1 && EXTRA_NVCCFLAGS="--std=c++11" || true
 rm -f $CONDA_PREFIX/test.out
 
 $SRC_DIR/build/linux/configure --with-install-type=prefix --with-cuda=$CUDA_ROOT --prefix=$CONDA_PREFIX NVCCFLAGS="-ccbin $CC -I$CONDA_PREFIX/include $EXTRA_NVCCFLAGS" CC=$CC CXX=$CXX CPPFLAGS="-I$CONDA_PREFIX/include"
