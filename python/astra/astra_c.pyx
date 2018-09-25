6730face204f40c57c3e8fe58ea9749494d669a6
Willem Jan Palenstijn <Willem.Jan.Palenstijn@cwi.nl>
2018-09-25 14:00:07 +0200

    Raise exception when setting GPU memory unrealistically small

--- python/astra/astra_c.pyx
+++ python/astra/astra_c.pyx
@@ -84,6 +84,8 @@ IF HAVE_CUDA==True:
     if use_cuda()==True:
         if not isinstance(idx, collections.Iterable) or isinstance(idx, six.string_types + (six.text_type,six.binary_type)):
             idx = (idx,)
+        if memory < 1024*1024:
+            raise ValueError("Setting GPU memory lower than 1MB is not supported.")
         params.memory = memory
         params.GPUIndices = idx
         setGlobalGPUParams(params)
