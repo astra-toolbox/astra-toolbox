6627f4ae2ce5262b2aafa39ea94c7dfd33fbc672
Willem Jan Palenstijn <Willem.Jan.Palenstijn@cwi.nl>
2019-04-12 11:07:48 +0200

    Update conda builds for cuda 10 and numpy 1.16

--- python/conda/linux_release/buildenv/build.sh
+++ python/conda/linux_release/buildenv/build.sh
@@ -5,9 +5,14 @@ export CXX=g++
 
 cd /root
 git clone --depth 1 --branch master https://github.com/astra-toolbox/astra-toolbox
-[ $# -eq 0 ] || perl -pi -e "s/^(\s*number:\s*)[0-9]+$/\${1}$1/" astra-toolbox/python/conda/libastra/meta.yaml astra-toolbox/python/conda/astra-toolbox/meta.yaml
+[ $# -eq 0 ] || perl -pi -e "s/^(\s*version:\s*)[0-9a-z+\.']+$/\${1}'$1'/" astra-toolbox/python/conda/libastra/meta.yaml astra-toolbox/python/conda/astra-toolbox/meta.yaml
+[ $# -eq 0 ] || perl -pi -e "s/^(\s*number:\s*)[0-9]+$/\${1}$2/" astra-toolbox/python/conda/libastra/meta.yaml astra-toolbox/python/conda/astra-toolbox/meta.yaml
+[ $# -eq 0 ] || perl -pi -e "s/^(\s*-\s*libastra\s*==\s*)[0-9a-z+\.]+$/\${1}$1/" astra-toolbox/python/conda/astra-toolbox/meta.yaml
+
 
 conda-build -m astra-toolbox/python/conda/libastra/linux_build_config.yaml astra-toolbox/python/conda/libastra
-conda-build astra-toolbox/python/conda/astra-toolbox
+for i in 27 35 36 37; do
+  conda-build -m astra-toolbox/python/conda/astra-toolbox/conda_build_config_py$i.yaml astra-toolbox/python/conda/astra-toolbox
+done
 
 cp /root/miniconda3/conda-bld/linux-64/*astra* /out
--- python/conda/linux_release/release.sh
+++ python/conda/linux_release/release.sh
@@ -2,7 +2,7 @@
 
 D=`mktemp -d`
 
-for F in https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh  http://developer.download.nvidia.com/compute/cuda/5_5/rel/installers/cuda_5.5.22_linux_64.run http://developer.download.nvidia.com/compute/cuda/6_0/rel/installers/cuda_6.0.37_linux_64.run  http://developer.download.nvidia.com/compute/cuda/7_0/Prod/local_installers/cuda_7.0.28_linux.run http://developer.download.nvidia.com/compute/cuda/7_0/Prod/cufft_update/cufft_patch_linux.tar.gz http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda_7.5.18_linux.run https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run https://developer.nvidia.com/compute/cuda/8.0/Prod2/patches/2/cuda_8.0.61.2_linux-run https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run https://developer.nvidia.com/compute/cuda/9.2/Prod2/local_installers/cuda_9.2.148_396.37_linux; do
+for F in https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh  http://developer.download.nvidia.com/compute/cuda/5_5/rel/installers/cuda_5.5.22_linux_64.run http://developer.download.nvidia.com/compute/cuda/6_0/rel/installers/cuda_6.0.37_linux_64.run  http://developer.download.nvidia.com/compute/cuda/7_0/Prod/local_installers/cuda_7.0.28_linux.run http://developer.download.nvidia.com/compute/cuda/7_0/Prod/cufft_update/cufft_patch_linux.tar.gz http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda_7.5.18_linux.run https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run https://developer.nvidia.com/compute/cuda/8.0/Prod2/patches/2/cuda_8.0.61.2_linux-run https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run https://developer.nvidia.com/compute/cuda/9.2/Prod2/local_installers/cuda_9.2.148_396.37_linux https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux; do
 	[ -f buildenv/`basename $F` ] || (cd buildenv; wget $F )
 done
 
@@ -10,7 +10,7 @@ docker build -t astra-build-env buildenv
 
 cp buildenv/build.sh $D
 
-docker run -v $D:/out:z astra-build-env /bin/bash /out/build.sh $1
+docker run -v $D:/out:z astra-build-env /bin/bash /out/build.sh 1.9.0.dev10 0
 
 rm -f $D/build.sh
 
