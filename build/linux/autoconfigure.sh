60614e88879012d806a3d7f4f25e604d236eb78b
Tim <tim.elberfeld@uantwerpen.be>
2019-05-28 10:39:52 +0200

    removed erroneously added files
    Signed-off-by: Tim <tim.elberfeld@uantwerpen.be>

--- build/linux/autoconfigure.sh
+++
@@ -1,6 +0,0 @@
-./configure \
-    --with-cuda=/usr/local/cuda-9.2\
-    --with-matlab=/usr/local/MATLAB/R2017b\
-    --with-python=/home/tim/anaconda3/bin/python\
-    --prefix=$HOME/astra\
-    --with-install-type=module
