9a58b7451179ed512f975bc4c90fb71f172250b9
Willem Jan Palenstijn <wjp@usecode.org>
2019-07-08 21:15:52 +0200

    Build updates for Debian 8 + CUDA10.1
    Debian 7 is EOL, and CUDA 10.1 doesn't support its version of glibc.
    
    Hardcoded conda=4.6.14 for now, since 4.7.5 seems to be downloading
    corrupted packages when running in docker/linux-64.

--- python/conda/libastra/build.sh
+++ python/conda/libastra/build.sh
@@ -22,7 +22,7 @@ $SRC_DIR/build/linux/autogen.sh
 NVCC=$CUDA_ROOT/bin/nvcc
 echo "int main(){return 0;}" > $CONDA_PREFIX/test.cu
 $NVCC $CONDA_PREFIX/test.cu -ccbin $CC --std=c++11 -o $CONDA_PREFIX/test.out > /dev/null 2>&1 && EXTRA_NVCCFLAGS="--std=c++11" || true
-rm -f $CONDA_PREFIX/test.out
+rm -f $CONDA_PREFIX/test.out $CONDA_PREFIX/test.cu
 
 $SRC_DIR/build/linux/configure --with-install-type=prefix --with-cuda=$CUDA_ROOT --prefix=$CONDA_PREFIX NVCCFLAGS="-ccbin $CC -I$CONDA_PREFIX/include $EXTRA_NVCCFLAGS" CC=$CC CXX=$CXX CPPFLAGS="-I$CONDA_PREFIX/include"
 
