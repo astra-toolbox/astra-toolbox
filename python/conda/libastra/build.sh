d621b2ceed23f4ecc945b00ffd6e859b70b89b50
Allard Hendriksen <allard@allard-dell-xps-9560>
2019-09-17 14:43:58 +0200

    Use conda compiler toolchain for conda builds
    For both libastra and astra-toolbox:
    
    1) We do not use script_env to set CC/CXX anymore, since the compilers
       are installed by conda.
    2) The build string is made useful by including either the python+numpy
       version or the cudatoolkit version that the package was built with.
    3) Some clean-up of build.sh in buildenv/
    
    For libastra:
    
    1) The libastra.so is built with the conda C/C++ compiler
       toolchain. This has two benefits:
       1) The rpath of libastra.so is set to $ORIGIN, which makes linking
          easier for dependent packages.
       2) libastra.so is linkable against ancient versions of glibc. With
          old versions of memcpy.
    2) The C/C++ compiler version is fixed to 5.4.0
    3) In libastra/build.sh, we rename $CONDA_PREFIX to
       $PREFIX. Apparently, this is how it is supposed to be done. For me,
       $CONDA_PREFIX was suddenly undefined. Why this was not a problem
       before, is unclear to me.
    4) The cudatoolkit runtime dependency is pinned with pin_compatible
    5) The libastra conda package now provides headers and .pc file. This
       is useful for building C++ packages that depend on astra.
    6) Remove some old code related to cudatoolkit<8.0.
    
    For astra-toolbox:
    
    1) astra-toolbox uses the conda-provided compilers
    2) The compilers are fixed to version 7.3
    3) Add boost to host requirements of astra-toolbox
    
    Notes on testing:
    
    - The libastra build has been tested with all versions of cudatoolkit
    - The astra-toolbox build has been tested with all provided versions
      of python after building a single cudatoolkit version of libastra.
    
    How to test this branch:
    
    - It should work by just editing
      `python/conda/linux_release/buildenv/build.sh`. Set
      BRANCH=CI-use-conda-c-compiler-toolchain
      URL=https://github.com/ahendriksen/astra-toolbox
    
      and run release.sh from the `python/conda/linux_release` directory.

--- python/conda/libastra/build.sh
+++ python/conda/libastra/build.sh
@@ -18,35 +18,27 @@ cd $SRC_DIR/build/linux
 
 $SRC_DIR/build/linux/autogen.sh
 
-# Add C++11 to compiler flags if nvcc supports it, mostly to work around a boost bug
+# Add C++11 to compiler flags, mostly to work around a boost bug.
+# Since we require cudatoolkit>=8.0 nvcc supports this.
 NVCC=$CUDA_ROOT/bin/nvcc
-echo "int main(){return 0;}" > $CONDA_PREFIX/test.cu
-$NVCC $CONDA_PREFIX/test.cu -ccbin $CC --std=c++11 -o $CONDA_PREFIX/test.out > /dev/null 2>&1 && EXTRA_NVCCFLAGS="--std=c++11" || true
-rm -f $CONDA_PREFIX/test.out $CONDA_PREFIX/test.cu
+EXTRA_NVCCFLAGS="--std=c++11"
 
-$SRC_DIR/build/linux/configure --with-install-type=prefix --with-cuda=$CUDA_ROOT --prefix=$CONDA_PREFIX NVCCFLAGS="-ccbin $CC -I$CONDA_PREFIX/include $EXTRA_NVCCFLAGS" CC=$CC CXX=$CXX CPPFLAGS="-I$CONDA_PREFIX/include"
+
+$SRC_DIR/build/linux/configure --with-install-type=prefix --with-cuda=$CUDA_ROOT --prefix=$PREFIX NVCCFLAGS="-ccbin $CC -I$PREFIX/include $EXTRA_NVCCFLAGS" CC=$CC CXX=$CXX CPPFLAGS="-I$PREFIX/include"
 
 # Clean, because we may be re-using this source tree when building
 # multiple variants of this conda package.
 make clean
 
-make -j $CPU_COUNT install-libraries
+make -j $CPU_COUNT
+make -j $CPU_COUNT install-dev
 
 
 test -d $CUDA_ROOT/lib64 && LIBPATH="$CUDA_ROOT/lib64" || LIBPATH="$CUDA_ROOT/lib"
 
 case `uname` in
   Darwin*)
-    cp -P $LIBPATH/libcudart.*.dylib $CONDA_PREFIX/lib
-    cp -P $LIBPATH/libcufft.*.dylib $CONDA_PREFIX/lib
-    ;;
-  Linux*)
-    if [ "$cudatoolkit" = "7.0" ]; then
-      # For some reason conda-build adds these symlinks automatically for
-      # cudatoolkit-5.5 and 6.0, but not 7.0. For 7.5 these symlinks are not
-      # necessary, and for 8.0 the cudatoolkit packages includes them.
-      ln -T -s libcudart.so.7.0.28 $CONDA_PREFIX/lib/libcudart.so.7.0
-      ln -T -s libcufft.so.7.0.35 $CONDA_PREFIX/lib/libcufft.so.7.0
-    fi
+    cp -P $LIBPATH/libcudart.*.dylib $PREFIX/lib
+    cp -P $LIBPATH/libcufft.*.dylib $PREFIX/lib
     ;;
 esac
