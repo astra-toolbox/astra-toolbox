82fc179f39ed6da308b0f229769f899570a1d9ea
Willem Jan Palenstijn <wjp@usecode.org>
2020-01-17 10:22:05 +0100

    Use numpy backward binary compatibility for conda packages
    We now only build a single package built with numpy 1.11.x for numpy>=1.11,<2 .
    
    There is no guarantee that every future numpy version covered by this
    will be compatible with numpy 1.11, but numpy checks this internally at
    run-time, and should report this (noisily) when this occurs.

--- python/conda/linux_release/buildenv/build.sh
+++ python/conda/linux_release/buildenv/build.sh
@@ -13,13 +13,12 @@ git clone --depth 1 --branch ${BRANCH} ${URL}
 
 [ $# -eq 0 ] || perl -pi -e "s/^(\s*version:\s*)[0-9a-z+\.']+$/\${1}'$1'/" astra-toolbox/python/conda/libastra/meta.yaml astra-toolbox/python/conda/astra-toolbox/meta.yaml
 [ $# -eq 0 ] || perl -pi -e "s/^(\s*number:\s*)[0-9]+$/\${1}$2/" astra-toolbox/python/conda/libastra/meta.yaml astra-toolbox/python/conda/astra-toolbox/meta.yaml
+[ $# -eq 0 ] || perl -pi -e "s/^(\s*string:.+_)[0-9]+/\${1}$2/" astra-toolbox/python/conda/libastra/meta.yaml
 [ $# -eq 0 ] || perl -pi -e "s/^(\s*-\s*libastra\s*==\s*)[0-9a-z+\.]+$/\${1}$1/" astra-toolbox/python/conda/astra-toolbox/meta.yaml
 
 
 conda-build -m astra-toolbox/python/conda/libastra/linux_build_config.yaml astra-toolbox/python/conda/libastra
 
-for i in 27 36 37 38; do
-  conda-build -m astra-toolbox/python/conda/astra-toolbox/conda_build_config_py$i.yaml astra-toolbox/python/conda/astra-toolbox
-done
+conda-build -m astra-toolbox/python/conda/astra-toolbox/linux_build_config.yaml astra-toolbox/python/conda/astra-toolbox
 
 cp /root/miniconda3/conda-bld/linux-64/*astra* /out
