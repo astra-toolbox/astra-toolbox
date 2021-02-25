6aa95260dfd5ead47963ad7d8275ae9f9ea6645a
Willem Jan Palenstijn <wjp@usecode.org>
2021-02-25 10:28:47 +0100

    Fix Windows conda package build

--- python/conda/astra-toolbox/bld.bat
+++ python/conda/astra-toolbox/bld.bat
@@ -29,5 +29,5 @@ cd python
 
 set VS90COMNTOOLS=%VS140COMNTOOLS%
 set CL=/DASTRA_CUDA /DASTRA_PYTHON "/I%R%\include" "/I%R%\lib\include" "/I%CUDA_PATH%\include"
-copy "%CONDA_PREFIX%\Library\lib\AstraCuda64.lib" astra.lib
+copy "%LIBRARY_LIB%\AstraCuda64.lib" astra.lib
 python builder.py build_ext --compiler=msvc install
