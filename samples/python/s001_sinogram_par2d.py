d877a9b81c692320a60cb723b523ad27e42826e5
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2021-03-15 20:52:06 +0100

    moved header to readme

--- samples/python/s001_sinogram_par2d.py
+++ samples/python/s001_sinogram_par2d.py
@@ -1,28 +1,3 @@
-# -----------------------------------------------------------------------
-# Copyright: 2010-2018, imec Vision Lab, University of Antwerp
-#            2013-2018, CWI, Amsterdam
-#
-# Contact: astra@astra-toolbox.com
-# Website: http://www.astra-toolbox.com/
-#
-# This file is part of the ASTRA Toolbox.
-#
-#
-# The ASTRA Toolbox is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# The ASTRA Toolbox is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with the ASTRA Toolbox. If not, see <http://www.gnu.org/licenses/>.
-#
-# -----------------------------------------------------------------------
-
 import astra
 import numpy as np
 
