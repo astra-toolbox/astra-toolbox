4a0b1791c4f840f4689790f8707d751d65176138
Willem Jan Palenstijn <Willem.Jan.Palenstijn@cwi.nl>
2021-10-11 11:53:47 +0200

    Windows Python release update

--- build/msvc/build_release.bat
+++ build/msvc/build_release.bat
@@ -13,8 +13,7 @@ mkdir release
 cd release
 mkdir src
 mkdir matlab
-mkdir python27
-mkdir python37
+mkdir python39
 
 cd src
 git clone -b %B_RELEASE_TAG% https://github.com/astra-toolbox/astra-toolbox astra-%B_RELEASE%
@@ -46,7 +45,7 @@ pause
 
 rem -------------------------------------------------------------------
 
-cd %R%\release\python27
+cd %R%\release\python39
 mkdir astra-%B_RELEASE%
 cd astra-%B_RELEASE%
 xcopy /e /i %R%\samples\python samples
@@ -56,52 +55,10 @@ copy %R%\COPYING COPYING.txt
 copy %B_VCREDIST% .
 
 mkdir astra
-call "%B_WINPYTHON2%\scripts\env.bat"
-copy %WINPYDIR%\lib\site-packages\astra\*.* astra
-copy %R%\bin\x64\Release_CUDA\AstraCuda64.lib astra
-copy "%CUDA_PATH%\bin\cudart64_90.dll" astra
-copy "%CUDA_PATH%\bin\cufft64_90.dll" astra
-
-(
-echo -----------------------------------------------------------------------
-echo This file is part of the ASTRA Toolbox
-echo.
-echo Copyright: 2010-2019, imec Vision Lab, University of Antwerp
-echo            2014-2019, CWI, Amsterdam
-echo            http://visielab.uantwerpen.be/ and http://www.cwi.nl/
-echo License: Open Source under GPLv3
-echo Contact: astra@astra-toolbox.com
-echo Website: http://www.astra-toolbox.com/
-echo -----------------------------------------------------------------------
-echo.
-echo.
-echo This directory contains pre-built Python modules for the ASTRA Toolbox.
-echo.
-echo It has been built with WinPython-64bit-%B_WP2%.
-echo.
-echo To use it, move the astra\ directory to your existing site-packages directory.
-echo Its exact location depends on your Python installation, but should look
-echo similar to %B_README_WP2% .
-echo.
-echo Sample code can be found in the samples\ directory.
-) > README.txt
-
-pause
-
-rem -------------------------------------------------------------------
-
-cd %R%\release\python37
-mkdir astra-%B_RELEASE%
-cd astra-%B_RELEASE%
-xcopy /e /i %R%\samples\python samples
-copy %R%\NEWS.txt .
-copy %R%\COPYING COPYING.txt
-
-copy %B_VCREDIST% .
-
-mkdir astra
+mkdir astra\plugins
 call "%B_WINPYTHON3%\scripts\env.bat"
 copy %WINPYDIR%\lib\site-packages\astra\*.* astra
+copy %WINPYDIR%\lib\site-packages\astra\plugins\*.* astra\plugins
 copy %R%\bin\x64\Release_CUDA\AstraCuda64.lib astra
 copy "%CUDA_PATH%\bin\cudart64_90.dll" astra
 copy "%CUDA_PATH%\bin\cufft64_90.dll" astra
@@ -110,8 +67,8 @@ copy "%CUDA_PATH%\bin\cufft64_90.dll" astra
 echo -----------------------------------------------------------------------
 echo This file is part of the ASTRA Toolbox
 echo.
-echo Copyright: 2010-2019, imec Vision Lab, University of Antwerp
-echo            2014-2019, CWI, Amsterdam
+echo Copyright: 2010-2021, imec Vision Lab, University of Antwerp
+echo            2014-2021, CWI, Amsterdam
 echo            http://visielab.uantwerpen.be/ and http://www.cwi.nl/
 echo License: Open Source under GPLv3
 echo Contact: astra@astra-toolbox.com
@@ -134,8 +91,7 @@ pause
 
 cd %R%\release
 python -c "import shutil; shutil.make_archive('astra-%B_RELEASE%-matlab-win-x64', 'zip', 'matlab')"
-python -c "import shutil; shutil.make_archive('astra-%B_RELEASE%-python27-win-x64', 'zip', 'python27')"
-python -c "import shutil; shutil.make_archive('astra-%B_RELEASE%-python37-win-x64', 'zip', 'python37')"
+python -c "import shutil; shutil.make_archive('astra-%B_RELEASE%-python39-win-x64', 'zip', 'python39')"
 python -c "import shutil; shutil.make_archive('astra-%B_RELEASE%', 'zip', 'src')"
 
 pause
