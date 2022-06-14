4b80f45cd49469301e720a01a16f6f694d1397a1
Daniel Ching <carterbox@users.noreply.github.com>
2022-06-14 16:35:43 -0500

    Resync with upstream

--- build/msvc/build_python3.bat
+++ build/msvc/build_python3.bat
@@ -7,7 +7,7 @@ set R=%CD%
 call "%~dp0build_env.bat"
 
 call "%B_WINPYTHON3%\scripts\env.bat"
-call "%B_VC%\vcvars64.bat"
+call "%B_VC%\vcvarsall.bat" amd64 8.1 -vcvars_ver=14.0
 
 cd /D %R%
 
@@ -16,12 +16,12 @@ msbuild astra_vc14.sln /p:Configuration=Release_CUDA /p:Platform=x64 /t:astra_vc
 cd python
 
 rd /s /q build
-rd /s /q "%B_WINPYTHON3%\lib\site-packages\astra"
+rd /s /q "%WINPYDIR%\lib\site-packages\astra"
 
 set CL=/DASTRA_CUDA /DASTRA_PYTHON
 set INCLUDE=%R%\include;%R%\lib\include;%CUDA_PATH%\include;%INCLUDE%
 copy ..\bin\x64\Release_CUDA\AstraCuda64.lib astra.lib
-%B_WINPYTHON3%\python builder.py build_ext --compiler=msvc install
-copy ..\bin\x64\Release_CUDA\AstraCuda64.dll "%B_WINPYTHON3%\lib\site-packages\astra"
+python builder.py build_ext --compiler=msvc install
+copy ..\bin\x64\Release_CUDA\AstraCuda64.dll "%WINPYDIR%\lib\site-packages\astra"
 
 pause
