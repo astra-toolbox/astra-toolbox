79690e9de227bf8a26b5eb5b4b6061b2bce9f16c
Willem Jan Palenstijn <wjp@usecode.org>
2022-06-30 16:36:23 +0200

    Build conda packages for cuda 11.5, 11.6, 11.7

--- python/conda/linux_release/release.sh
+++ python/conda/linux_release/release.sh
@@ -29,7 +29,9 @@ for F in \
  https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda_11.2.2_460.32.03_linux.run \
  https://developer.download.nvidia.com/compute/cuda/11.3.1/local_installers/cuda_11.3.1_465.19.01_linux.run \
  https://developer.download.nvidia.com/compute/cuda/11.4.1/local_installers/cuda_11.4.1_470.57.02_linux.run \
- https://developer.download.nvidia.com/compute/cuda/11.5.0/local_installers/cuda_11.5.0_495.29.05_linux.run \
+ https://developer.download.nvidia.com/compute/cuda/11.5.1/local_installers/cuda_11.5.1_495.29.05_linux.run \
+ https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda_11.6.0_510.39.01_linux.run \
+ https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda_11.7.0_515.43.04_linux.run \
 ; do
 	[ -f buildenv.deb11/`basename $F` ] || (cd buildenv.deb11; wget $F )
 done
