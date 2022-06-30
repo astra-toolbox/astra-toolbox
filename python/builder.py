36af0f174fbe470e7912509f8c3f56333113027f
Willem Jan Palenstijn <Willem.Jan.Palenstijn@cwi.nl>
2022-06-30 10:38:04 +0200

    Version 2.1.1

--- python/builder.py
+++ python/builder.py
@@ -74,7 +74,7 @@ for m in ext_modules:
                                       'PythonPluginAlgorithmFactory.cpp'))
 
 setup(name='astra-toolbox',
-      version='2.1.0',
+      version='2.1.1',
       description='Python interface to the ASTRA Toolbox',
       author='D.M. Pelt',
       author_email='D.M.Pelt@cwi.nl',
