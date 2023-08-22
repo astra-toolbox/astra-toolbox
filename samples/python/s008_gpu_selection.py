1d06bd5a70d42cffc44abb0b12a1f384f351ca5f
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2023-08-22 21:52:17 +0200

    Update s008_gpu_selection

--- samples/python/s008_gpu_selection.py
+++ samples/python/s008_gpu_selection.py
@@ -26,6 +26,9 @@
 import astra
 import numpy as np
 
+# Switch GPU to GPU #1. The default is #0.
+astra.set_gpu_index(1)
+
 vol_geom = astra.create_vol_geom(256, 256)
 proj_geom = astra.create_proj_geom('parallel', 1.0, 384, np.linspace(0,np.pi,180,False))
 import scipy.io
@@ -33,8 +36,8 @@ P = scipy.io.loadmat('phantom.mat')['phantom256']
 
 proj_id = astra.create_projector('cuda',proj_geom,vol_geom)
 
-# Create a sinogram from a phantom, using GPU #1. (The default is #0)
-sinogram_id, sinogram = astra.create_sino(P, proj_id, gpuIndex=1)
+# Create a sinogram from a phantom
+sinogram_id, sinogram = astra.create_sino(P, proj_id)
 
 
 # Set up the parameters for a reconstruction algorithm using the GPU
@@ -42,10 +45,6 @@ rec_id = astra.data2d.create('-vol', vol_geom)
 cfg = astra.astra_dict('SIRT_CUDA')
 cfg['ReconstructionDataId'] = rec_id
 cfg['ProjectionDataId'] = sinogram_id
-
-# Use GPU #1 for the reconstruction. (The default is #0.)
-cfg['option'] = {}
-cfg['option']['GPUindex'] = 1
 
 # Run 150 iterations of the algorithm
 alg_id = astra.algorithm.create(cfg)
