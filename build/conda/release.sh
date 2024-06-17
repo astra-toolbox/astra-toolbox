79d5b521493ed8ec511be9b470d61866c0d08dbf
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2024-06-17 15:33:22 +0200

    Update conda packaging for python 3.12

--- build/conda/release.sh
+++ build/conda/release.sh
@@ -10,7 +10,7 @@ B=0
 
 podman run --rm -v $D:/out:z astra-build-deb9 /bin/bash /out/build.sh $V $B deb9 full
 podman run --rm -v $D:/out:z astra-build-deb11 /bin/bash /out/build.sh $V $B deb11 full
-podman run --rm -v $D:/out:z astra-build-deb12 /bin/bash /out/build.sh $V $B deb12
+podman run --rm -v $D:/out:z astra-build-deb12 /bin/bash /out/build.sh $V $B deb12 full
 
 rm -f $D/build.sh
 
