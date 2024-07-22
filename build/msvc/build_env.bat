8f6a38232dac5ab938ababb2519b5df5887a9c6f
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2024-07-22 14:18:32 +0200

    Remove boost from Windows build files

--- build/msvc/build_env.bat
+++ build/msvc/build_env.bat
@@ -2,8 +2,6 @@ set B_WP3=3.12.2
 set B_WINPYTHON3=C:\Python312
 set B_MATLAB_ROOT=C:\Users\Vagrant\MATLAB\R2023b
 set B_VC=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\
-set B_BV=1_74
-set B_BOOST=C:\local\boost_%B_BV%_0
 
 set B_VCREDIST=C:\Users\Vagrant\vc_redist.x64.exe
 
