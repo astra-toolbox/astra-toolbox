e43c56787299c5b0925bc40e1c39a750006e8d4b
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2024-07-29 17:12:21 +0200

    Fix usage of np.inf for numpy 2.0

--- python/astra/plugins/sirt.py
+++ python/astra/plugins/sirt.py
@@ -56,12 +56,12 @@ class SIRTPlugin(astra.plugin.base):
             self.data_mod = astra.data3d
 
         self.R = self.W * np.ones(v.shape,dtype=np.float32).ravel();
-        self.R[self.R < 0.000001] = np.Inf
+        self.R[self.R < 0.000001] = np.inf
         self.R = 1 / self.R
         self.R = self.R.reshape(s.shape)
 
         self.mrC = self.W.T * np.ones(s.shape,dtype=np.float32).ravel();
-        self.mrC[self.mrC < 0.000001] = np.Inf
+        self.mrC[self.mrC < 0.000001] = np.inf
         self.mrC = -Relaxation / self.mrC
         self.mrC = self.mrC.reshape(v.shape)
         
