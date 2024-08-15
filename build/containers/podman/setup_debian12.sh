41f5a2eca3e4c24c24a3274b12420b67e4db9412
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2024-08-15 13:42:03 +0200

    Add CUDA 12.6 to build environment

--- build/containers/podman/setup_debian12.sh
+++ build/containers/podman/setup_debian12.sh
@@ -4,15 +4,15 @@ set -e
 
 . util.sh
 
-setup_download $MINICONDA $CUDA120 $CUDA121 $CUDA122 $CUDA123 $CUDA124 $CUDA125
+setup_download $MINICONDA $CUDA120 $CUDA121 $CUDA122 $CUDA123 $CUDA124 $CUDA125 $CUDA126
 
 ctr=$(buildah from debian:12)
 
 setup_base
 setup_conda
-setup_cuda $CUDA120 $CUDA121 $CUDA122 $CUDA123 $CUDA124 $CUDA125
-setup_cache_conda cuda-cudart 12.0 12.1 12.2 12.3 12.4 12.5
-setup_cache_conda libcufft 11.0.0 11.0.1 11.0.2 11.0.8 11.0.12 11.2.1 11.2.3
+setup_cuda $CUDA120 $CUDA121 $CUDA122 $CUDA123 $CUDA124 $CUDA125 $CUDA126
+setup_cache_conda cuda-cudart 12.0 12.1 12.2 12.3 12.4 12.5 12.6
+setup_cache_conda libcufft 11.0.0 11.0.1 11.0.2 11.0.8 11.0.12 11.2.1 11.2.3 11.2.6
 
 buildah commit $ctr astra-build-deb12
 
