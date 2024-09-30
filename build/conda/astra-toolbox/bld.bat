6936a92e0632b06e5b60be41eaf03392a91f9d04
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2024-09-30 12:29:46 +0200

    Enable c++17 in Windows Python builds

--- build/conda/astra-toolbox/bld.bat
+++ build/conda/astra-toolbox/bld.bat
@@ -6,6 +6,6 @@ cd /D %R%
 
 cd python
 
-set CL=/DASTRA_CUDA /DASTRA_PYTHON "/I%R%\include" "/I%R%\lib\include" "/I%CUDA_PATH%\include"
+set CL=/DASTRA_CUDA /DASTRA_PYTHON "/I%R%\include" "/I%R%\lib\include" "/I%CUDA_PATH%\include" /std:c++17
 copy "%LIBRARY_LIB%\AstraCuda64.lib" astra.lib
 python builder.py build_ext --compiler=msvc install
