27ec8115aeef8ac48c681dad0bdfc762f00f840a
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2024-11-11 11:01:14 +0100

    Ensure we hold the GIL while processing python objects
    There should currently be no cases where this code is called without
    holding the GIL, but that is likely to change in the future with
    Python 3.13's free-threading option.

--- python/astra/CFloat32CustomPython.h
+++ python/astra/CFloat32CustomPython.h
@@ -1,7 +1,21 @@
+class GILHelper {
+public:
+    GILHelper() {
+        state = PyGILState_Ensure();
+    }
+    ~GILHelper() {
+        PyGILState_Release(state);
+    }
+private:
+    PyGILState_STATE state;
+};
+
 class CFloat32CustomPython : public astra::CFloat32CustomMemory {
 public:
     CFloat32CustomPython(PyArrayObject * arrIn)
     {
+        GILHelper gil; // hold GIL during this function
+
         arr = arrIn;
         // Set pointer to numpy data pointer
         m_fPtr = (float *)PyArray_DATA(arr);
@@ -9,6 +23,8 @@ public:
         Py_INCREF(arr);
     }
     virtual ~CFloat32CustomPython() {
+        GILHelper gil; // hold GIL during this function
+
         // Decrease reference count since ASTRA object is destroyed
         Py_DECREF(arr);
     }
@@ -21,6 +37,8 @@ class CDataStoragePython : public astra::CDataMemory<T> {
 public:
     CDataStoragePython(PyArrayObject *arrIn)
     {
+        GILHelper gil; // hold GIL during this function
+
         arr = arrIn;
         // Set pointer to numpy data pointer
         this->m_pfData = (T *)PyArray_DATA(arr);
@@ -28,6 +46,8 @@ public:
         Py_INCREF(arr);
     }
     virtual ~CDataStoragePython() {
+        GILHelper gil; // hold GIL during this function
+
         // Decrease reference count since ASTRA object is destroyed
         Py_DECREF(arr);
 
