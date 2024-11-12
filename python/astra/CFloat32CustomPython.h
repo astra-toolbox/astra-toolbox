55445dba73f4e28c690c6e03e93b1bc9f37133ac
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2024-11-12 11:35:30 +0100

    Remove no longer used code

--- python/astra/CFloat32CustomPython.h
+++ python/astra/CFloat32CustomPython.h
@@ -31,30 +31,3 @@ public:
 private:
     PyArrayObject* arr;
 };
-
-template<typename T>
-class CDataStoragePython : public astra::CDataMemory<T> {
-public:
-    CDataStoragePython(PyArrayObject *arrIn)
-    {
-        GILHelper gil; // hold GIL during this function
-
-        arr = arrIn;
-        // Set pointer to numpy data pointer
-        this->m_pfData = (T *)PyArray_DATA(arr);
-        // Increase reference count since ASTRA has a reference
-        Py_INCREF(arr);
-    }
-    virtual ~CDataStoragePython() {
-        GILHelper gil; // hold GIL during this function
-
-        // Decrease reference count since ASTRA object is destroyed
-        Py_DECREF(arr);
-
-        this->m_pfData = nullptr;
-    }
-
-
-private:
-    PyArrayObject* arr;
-};
