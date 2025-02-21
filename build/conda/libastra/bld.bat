267fc0d40ff8ec88544eb28b6711e77b92c7cdf1
Alexander <alexander.skorikov@gmail.com>
2025-02-21 08:25:43 +0000

    Add parallel execution of msbuild

--- build/conda/libastra/bld.bat
+++ build/conda/libastra/bld.bat
@@ -5,7 +5,7 @@ set R=%SRC_DIR%
 cd /D %R%/build/msvc
 python gen.py %cudatoolkit%
 
-msbuild astra_vc14.sln /p:Configuration=Release_CUDA /p:Platform=x64 /t:astra_vc14
+msbuild astra_vc14.sln /p:Configuration=Release_CUDA /p:Platform=x64 /t:astra_vc14 /maxcpucount:20
 
 copy bin\x64\Release_CUDA\AstraCuda64.dll "%LIBRARY_BIN%"
 copy bin\x64\Release_CUDA\AstraCuda64.lib "%LIBRARY_LIB%"
