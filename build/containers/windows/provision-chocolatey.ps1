bb0a10f61bf00aeeeb93484475949daa5d2dcc34
Alexander Skorikov <alexander.skorikov@gmail.com>
2025-04-18 15:39:16 +0200

    Update Windows VM provisioning scripts

--- build/containers/windows/provision-chocolatey.ps1
+++ build/containers/windows/provision-chocolatey.ps1
@@ -1,1 +1,2 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
--- build/containers/windows/provision-python.ps1
+++ build/containers/windows/provision-python.ps1
@@ -1,8 +1,16 @@
-choco install -y python3 --version=3.12.2
+choco install -y python39 --version=3.9.13
+choco install -y python310 --version=3.10.11
+choco install -y python311 --version=3.11.9
+choco install -y python312 --version=3.12.4
+choco install -y python313 --version=3.13.2
+
+C:\python39\python -m pip install numpy scipy cython setuptools wheel
+C:\python310\python -m pip install numpy scipy cython setuptools wheel
+C:\python311\python -m pip install numpy scipy cython setuptools wheel
+C:\python312\python -m pip install numpy scipy cython setuptools wheel
+C:\python313\python -m pip install numpy scipy cython setuptools wheel
+
 choco install -y miniconda3
-
-C:\python312\python -m pip install numpy scipy six cython setuptools
-
 C:\tools\miniconda3\shell\condabin\conda-hook.ps1
 conda activate base
 conda install -y conda-build conda-verify
