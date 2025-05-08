b488164d259bc3ccd42b66ed8d2a8d2edc0c62ff
Alexander Skorikov <alexander.skorikov@gmail.com>
2025-05-08 10:22:55 +0200

    Fix CUDA dependencies not found when installed from PyPI

--- build/pypi/build.sh
+++ build/pypi/build.sh
@@ -4,6 +4,7 @@ set -e
 
 BRANCH=master
 URL=https://github.com/astra-toolbox/astra-toolbox
+CUDA_RPATHS='$ORIGIN/../nvidia/cuda_runtime/lib:$ORIGIN/../nvidia/cufft/lib'
 
 echo "Cloning from ${URL}"
 echo "        branch: ${BRANCH}"
@@ -14,28 +15,14 @@ git clone --depth 1 --branch ${BRANCH} ${URL}
 cd astra-toolbox/build/linux
 
 ./autogen.sh
-
-./configure --with-python=python3.9 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module --with-python-wheel-platform=manylinux2014_x86_64
-make -j 20 all python-wheel
-cp python/dist/*.whl /out
-make clean
-
-./configure --with-python=python3.10 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module --with-python-wheel-platform=manylinux2014_x86_64
-make -j 20 all python-wheel
-cp python/dist/*.whl /out
-make clean
-
-./configure --with-python=python3.11 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module --with-python-wheel-platform=manylinux2014_x86_64
-make -j 20 all python-wheel
-cp python/dist/*.whl /out
-make clean
-
-./configure --with-python=python3.12 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module --with-python-wheel-platform=manylinux2014_x86_64
-make -j 20 all python-wheel
-cp python/dist/*.whl /out
-make clean
-
-./configure --with-python=python3.13 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module --with-python-wheel-platform=manylinux2014_x86_64
-make -j 20 all python-wheel
-cp python/dist/*.whl /out
-make clean
+for PYTHON_VERSION in 3.9 3.10 3.11 3.12 3.13; do
+    ./configure --with-python=python${PYTHON_VERSION} \
+                --with-cuda=/usr/local/cuda-12.1 \
+                --with-install-type=module \
+                --with-python-wheel-platform=manylinux2014_x86_64
+    make -j 20 libastra.la
+    patchelf --set-rpath $CUDA_RPATHS .libs/$(source libastra.la; echo $dlname)
+    make python-wheel
+    cp python/dist/*.whl /out
+    make clean
+done
