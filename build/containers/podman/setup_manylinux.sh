53bf93dbc0abbf308ac5591ac0e75ab3d2ead32d
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2025-05-09 15:48:10 +0200

    Add Python 3.13 to manylinux build container

--- build/containers/podman/setup_manylinux.sh
+++ build/containers/podman/setup_manylinux.sh
@@ -10,11 +10,12 @@ ctr=$(buildah from quay.io/pypa/manylinux2014_x86_64:latest)
 
 setup_cuda $CUDA121
 
-buildah run $ctr manylinux-interpreters ensure cp39-cp39 cp310-cp310 cp311-cp311 cp312-cp312
+buildah run $ctr manylinux-interpreters ensure cp39-cp39 cp310-cp310 cp311-cp311 cp312-cp312 cp313-cp313
 setup_pip_install python3.9 setuptools wheel numpy six Cython scipy
 setup_pip_install python3.10 setuptools wheel numpy six Cython scipy
 setup_pip_install python3.11 setuptools wheel numpy six Cython scipy
 setup_pip_install python3.12 setuptools wheel numpy six Cython scipy
+setup_pip_install python3.13 setuptools wheel numpy six Cython scipy
 
 buildah commit $ctr astra-build-manylinux
 
