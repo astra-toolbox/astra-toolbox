81398defb847fad0d160eba6289f5990864dfa25
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2025-05-09 14:28:39 +0200

    Switch to setup.py/pyproject.toml build system
    This refactors the Python build system to be mostly compatible with
    modern Python packaging standards, removing the deprecated direct
    invocations of a setuptools script.
    
    Since our Python package depends on the C++ library being built first,
    setup.py checks that the environment variable ASTRA_CONFIG is set,
    and our various build scripts set this appropriately.
    
    The --with-python-wheel-platform option to configure has been removed,
    and our PyPI release script now uses auditwheel to set that.
    
    This depends on (either of) the tomllib (included in Python 3.11+) or
    tomli Python modules to parse custom tool options in our pyproject.toml.

--- build/pypi/build.sh
+++ build/pypi/build.sh
@@ -15,27 +15,32 @@ cd astra-toolbox/build/linux
 
 ./autogen.sh
 
-./configure --with-python=python3.9 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module --with-python-wheel-platform=manylinux2014_x86_64
-make -j 20 all python-wheel
-cp python/dist/*.whl /out
+./configure --with-python=python3.9 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module
+make -j 20 ASTRA_CONFIG=pypi_linux_cuda all
+auditwheel repair --plat manylinux2014_x86_64 --exclude "libcudart.so.*" --exclude "libcufft.so.*" python/dist/*.whl
+mv wheelhouse/*.whl /out
 make clean
 
-./configure --with-python=python3.10 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module --with-python-wheel-platform=manylinux2014_x86_64
-make -j 20 all python-wheel
-cp python/dist/*.whl /out
+./configure --with-python=python3.10 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module
+make -j 20 ASTRA_CONFIG=pypi_linux_cuda all
+auditwheel repair --plat manylinux2014_x86_64 --exclude "libcudart.so.*" --exclude "libcufft.so.*" python/dist/*.whl
+mv wheelhouse/*.whl /out
 make clean
 
-./configure --with-python=python3.11 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module --with-python-wheel-platform=manylinux2014_x86_64
-make -j 20 all python-wheel
-cp python/dist/*.whl /out
+./configure --with-python=python3.11 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module
+make -j 20 ASTRA_CONFIG=pypi_linux_cuda all
+auditwheel repair --plat manylinux2014_x86_64 --exclude "libcudart.so.*" --exclude "libcufft.so.*" python/dist/*.whl
+mv wheelhouse/*.whl /out
 make clean
 
-./configure --with-python=python3.12 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module --with-python-wheel-platform=manylinux2014_x86_64
-make -j 20 all python-wheel
-cp python/dist/*.whl /out
+./configure --with-python=python3.12 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module
+make -j 20 ASTRA_CONFIG=pypi_linux_cuda all
+auditwheel repair --plat manylinux2014_x86_64 --exclude "libcudart.so.*" --exclude "libcufft.so.*" python/dist/*.whl
+mv wheelhouse/*.whl /out
 make clean
 
-./configure --with-python=python3.13 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module --with-python-wheel-platform=manylinux2014_x86_64
-make -j 20 all python-wheel
-cp python/dist/*.whl /out
+./configure --with-python=python3.13 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module
+make  -j 20 ASTRA_CONFIG=pypi_linux_cuda all
+auditwheel repair --plat manylinux2014_x86_64 --exclude "libcudart.so.*" --exclude "libcufft.so.*" python/dist/*.whl
+mv wheelhouse/*.whl /out
 make clean
