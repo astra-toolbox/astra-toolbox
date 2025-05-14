e95f2c505385da2aadfb801032a224392445ca4a
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2025-05-14 15:47:04 +0200

    Upgrade auditwheel in manylinux build container
    This is required for the auditwheel repair --exclude argument to support
    wildcards.

--- build/containers/podman/setup_manylinux.sh
+++ build/containers/podman/setup_manylinux.sh
@@ -11,6 +11,7 @@ ctr=$(buildah from quay.io/pypa/manylinux2014_x86_64:latest)
 setup_cuda $CUDA121
 
 buildah run $ctr manylinux-interpreters ensure cp39-cp39 cp310-cp310 cp311-cp311 cp312-cp312 cp313-cp313
+buildah run $ctr pipx upgrade auditwheel
 setup_pip_install python3.9 setuptools wheel numpy six Cython scipy
 setup_pip_install python3.10 setuptools wheel numpy six Cython scipy
 setup_pip_install python3.11 setuptools wheel numpy six Cython scipy
