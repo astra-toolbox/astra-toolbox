b050ea62de8105116693da010bb0b00efe3d20c4
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2025-08-14 12:33:53 +0200

    Make clog initialization thread-safe

--- include/astra/Logging.h
+++ include/astra/Logging.h
@@ -53,7 +53,6 @@ class _AstraExport CLogger
   static bool m_bEnabledFile;
   static bool m_bEnabledScreen;
   static bool m_bFileProvided;
-  static bool m_bInitialized;
   static std::string m_sLastErrMsg;
   static void _assureIsInitialized();
   static void _setLastErrMsg(const char *sfile, int sline, const char *fmt, va_list ap);
