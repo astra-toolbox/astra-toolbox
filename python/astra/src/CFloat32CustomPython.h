c7e3a453d3d2fae0c9a62979180facc53bcc4c55
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2025-08-14 11:37:35 +0200

    Add note

--- python/astra/src/CFloat32CustomPython.h
+++ python/astra/src/CFloat32CustomPython.h
@@ -28,6 +28,13 @@ along with the ASTRA Toolbox. If not, see <http://www.gnu.org/licenses/>.
 #ifndef ASTRA_PYTHON_SRC_FLOAT32CUSTOMPYTHON_H
 #define ASTRA_PYTHON_SRC_FLOAT32CUSTOMPYTHON_H
 
+// TODO: Do we need to toggle this at compile-time or at run-time?
+
+// TODO: Check this on Windows
+// https://docs.python.org/3/howto/free-threading-extensions.html
+// claims 'On Windows, this macro is not defined automatically, but must be
+// specified to the compiler when building.'
+
 #ifndef Py_GIL_DISABLED
 
 class GILHelper {
