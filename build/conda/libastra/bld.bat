8b9dc7483a8afe1e323ff5751883e83d43f27127
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2025-12-17 13:36:48 +0100

    Add visual studio version switch

--- build/conda/libastra/bld.bat
+++ build/conda/libastra/bld.bat
@@ -3,7 +3,7 @@
 set R=%SRC_DIR%
 
 cd /D %R%/build/msvc
-python gen.py vs2017 %cudatoolkit_build%
+python gen.py %c_compiler% %cudatoolkit_build%
 
 msbuild astra.sln /p:Configuration=Release_CUDA /p:Platform=x64 /t:astra /maxcpucount:20
 
