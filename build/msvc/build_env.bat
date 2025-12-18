e6c1cd69c04aff7a711e85ebe0d545dfba5d2592
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2025-12-18 11:02:39 +0100

    Switch base dev tools to VS2022 in Windows build env

--- build/msvc/build_env.bat
+++ build/msvc/build_env.bat
@@ -1,7 +1,7 @@
 set B_WP3=3.13.11
 set B_WINPYTHON3=C:\Python313
 set B_MATLAB_ROOT=C:\Users\Vagrant\MATLAB\R2023b
-set B_VC=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\
+set B_VC=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\
 
 set B_VCREDIST=C:\Users\Vagrant\vc_redist.x64.exe
 
