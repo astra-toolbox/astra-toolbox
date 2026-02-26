f02c1cb775f284ec027278a165f2f2b9937a685f
Alexander Skorikov <alexander.skorikov@gmail.com>
2026-02-26 08:59:50 -0800

    Fix Windows SDK version when several are installed
    Otherwise, setuptools might pick up the SDK version
    different from what was used to build the shared lib,
    leading to potential errors.

--- build/msvc/build_python3.bat
+++ build/msvc/build_python3.bat
@@ -6,7 +6,7 @@ set R=%CD%
 
 call "%~dp0build_env.bat"
 
-call "%B_VC%\vcvars64.bat"
+call "%B_VC%\vcvars64.bat" 10.0.22621.0
 
 cd /D %~dp0
 
@@ -20,6 +20,7 @@ rd /s /q "%B_WINPYTHON3%\lib\site-packages\astra"
 
 set CL=/DASTRA_CUDA /DASTRA_BUILDING_CUDA /DASTRA_PYTHON /std:c++17
 set INCLUDE=%R%\include;%R%\lib\include;%CUDA_PATH%\include;%INCLUDE%
+set DISTUTILS_USE_SDK=1
 set ASTRA_CONFIG=windows_cuda
 copy ..\build\msvc\bin\x64\Release_CUDA\AstraCuda64.lib astra.lib
 copy ..\build\msvc\bin\x64\Release_CUDA\AstraCuda64.dll astra
