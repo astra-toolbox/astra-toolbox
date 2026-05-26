144988f8215952f8d4db5bb9c8f1ab510e2bc8a5
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2026-05-26 21:14:17 +0200

    Update manylinux platform

--- build/pypi/build.sh
+++ build/pypi/build.sh
@@ -22,7 +22,7 @@ for PYTHON_VERSION in 3.10 3.11 3.12 3.13 3.14; do
     make -j 20 ASTRA_CONFIG=pypi_linux_cuda libastra.la
     patchelf --set-rpath $CUDA_RPATHS .libs/$(source libastra.la; echo $dlname)
     make -j 20 ASTRA_CONFIG=pypi_linux_cuda py
-    auditwheel repair --plat manylinux2014_x86_64 --exclude "libcudart.so.*" --exclude "libcufft.so.*" python/dist/*.whl
+    auditwheel repair --plat manylinux_2_28_x86_64 --exclude "libcudart.so.*" --exclude "libcufft.so.*" python/dist/*.whl
     mv wheelhouse/*.whl /out
     make clean
 done
