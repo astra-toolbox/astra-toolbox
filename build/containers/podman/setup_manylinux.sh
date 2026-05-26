3e7e45b0607e86f815c8fc11d246a825a2c707d5
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2026-05-26 20:35:37 +0200

    Update manylinux build container to 2.28
    Scipy/numpy wheels are no longer supporting older versions.

--- build/containers/podman/setup_manylinux.sh
+++ build/containers/podman/setup_manylinux.sh
@@ -5,21 +5,20 @@ set -e
 . util.sh
 
 setup_download $CUDA129
-setup_download $CUDA130
+setup_download $CUDA132
 
-ctr=$(buildah from quay.io/pypa/manylinux2014_x86_64:latest)
+ctr=$(buildah from quay.io/pypa/manylinux_2_28_x86_64:latest)
 
 setup_cuda $CUDA129
-setup_cuda $CUDA130
+setup_cuda $CUDA132
 
-buildah run $ctr manylinux-interpreters ensure cp39-cp39 cp310-cp310 cp311-cp311 cp312-cp312 cp313-cp313 cp313-cp313t cp314-cp314 cp314-cp314t
+buildah run $ctr manylinux-interpreters ensure cp39-cp39 cp310-cp310 cp311-cp311 cp312-cp312 cp313-cp313 cp314-cp314 cp314-cp314t
 buildah run $ctr pipx upgrade auditwheel
 setup_pip_install python3.9 setuptools wheel numpy six Cython scipy
 setup_pip_install python3.10 setuptools wheel numpy six Cython scipy
 setup_pip_install python3.11 setuptools wheel numpy six Cython scipy
 setup_pip_install python3.12 setuptools wheel numpy six Cython scipy
 setup_pip_install python3.13 setuptools wheel numpy six Cython scipy
-setup_pip_install python3.13t setuptools wheel numpy six Cython scipy
 setup_pip_install python3.14 setuptools wheel numpy six Cython scipy
 setup_pip_install python3.14t setuptools wheel numpy six Cython scipy
 
